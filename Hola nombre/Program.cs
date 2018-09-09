f36697d5b741e25eab04168d586fa87d12a5b498
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-09 01:19:47 -0300

    HolaMartin

--- Hola nombre/Program.cs
+++ Hola nombre/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Hola_nombre
 {
-    class Program
+    public  class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*crear un programa que te pida el nombre al usuario y si es 
+             * martin le diga hola Martin , si no que le diga no te conozco.*/
+            string usuario = "Martin",usuario1;
+
+            Console.WriteLine("Ingrese su nombre de usuario: ");
+            usuario1 = Console.ReadLine();
+            if (usuario == usuario1)
+            {
+                Console.WriteLine("Hola Martin");
+            }
+            else
+            {
+                Console.WriteLine(" Oyeeee ..No te conozco!!!!!!!{0}",usuario1);
+            }
+            Console.ReadKey();
+            
+
+
+
+
+
         }
     }
 }
