5af8bdb712c18808db644b665c3ab5c774759801
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-09 01:01:55 -0300

    Agregar archivos de proyecto.

---
+++ Hola nombre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hola_nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
